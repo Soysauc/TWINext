de4b66211fe12bbe49fb619563a784a074155fea
Jeremy M Rivera <fangman09@gmail.com>
2023-04-13 14:36:17 -0400

    removed unused deps

--- src/pages/colors.css.ts
+++
@@ -1,8 +0,0 @@
-export const colors = {
-  background: "#fff",
-  text: "#004ca3",
-  primary: "#072AC6",
-  muted: "#072AC6",
-  active: "#072AC6",
-  black: "#000",
-}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import Footer from './components/footer';
-import Header from './components/header';
+// import Header from './components/header';
 export default function Home() {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <h1>Hello World</h1>
       <h2>Please</h2>
       <h3>Ok</h3>
